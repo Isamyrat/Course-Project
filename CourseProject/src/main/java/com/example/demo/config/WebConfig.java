7092a8b6ad6a67167cc46b8b51b05e57d00e335c
Isamyrat <kurbanowisa07@gmail.com>
2021-04-30 19:31:32 +0300

    I change my localization

--- CourseProject/src/main/java/com/example/demo/config/WebConfig.java
+++ CourseProject/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,18 +1,14 @@
 package com.example.demo.config;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 @Configuration
@@ -20,23 +16,32 @@ import java.util.Locale;
 @ComponentScan(basePackages = "com.example.demo.controller")
 public class WebConfig  implements WebMvcConfigurer {
 
-    @Bean("messageSource")
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource=new ReloadableResourceBundleMessageSource();
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource  = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:/locale/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
     }
 
- /*   @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }*/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor changeInterceptor = new LocaleChangeInterceptor();
+        changeInterceptor.setParamName("lang");
+        return changeInterceptor;
+    }
+
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -47,21 +52,7 @@ public class WebConfig  implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login/login");
     }
-    @Bean
-    public AcceptHeaderLocaleResolver localeResolver(WebMvcProperties mvcProperties) {
-        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver() {
-            @Override
-            public Locale resolveLocale(HttpServletRequest request) {
-                String locale = request.getParameter("lang");
-                return locale != null
-                        ? org.springframework.util.StringUtils.parseLocaleString(locale)
-                        : super.resolveLocale(request);
-            }
-        };
 
-        localeResolver.setDefaultLocale(mvcProperties.getLocale());
-        return localeResolver;
-    }
 
 
 }
